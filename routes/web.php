d588621d032589f1e8bdcf2a4630f00b4e26d752
senbiki <akifumi.senbiki.1209@gmail.com>
2022-08-20 16:09:52 +0900

    不要と思われるルーティングをコメントアウト

--- routes/web.php
+++ routes/web.php
@@ -28,49 +28,60 @@ Route::get('/', function () {
 // -------------------------------------------------------------------------
 Route::group(["prefix" => "line", "as" => "line."], function () {
 
-    // LINEログインが可能なアカウント一覧を表示
-    Route::get("/login", [LoginController::class, "index"])->name("login.index");
-    // ログイン用コントローラー
-    Route::get("/login/{line_account_id}/{application_key}", [LoginController::class, "detail"])->name("login.detail");
-    // ---------------------------------------------------------------
-    // LINEドメイン側で認証後に戻ってくるweb側のコールバックURL
-    // GETパラメータで line_account_idというキーでline_accounts.idを渡す
-    // ---------------------------------------------------------------
-    Route::get("/callback", [CallbackController::class, "index"])->name("callback.index");
-    // ---------------------------------------------------------------
-    // LINE認証完了後,DBを更新後表示するLINEログイン完了ページ
-    // GETパラメータで line_account_idというキーでline_accounts.idを渡す
-    // ---------------------------------------------------------------
-    Route::get("/callback/completed", [CallbackController::class, "completed"])->name("callback.completed");
+    Route::group(["prefix" => "login", "as" => "login."], function () {
+        // LINEログインが可能なアカウント一覧を表示
+        Route::get("/login", [
+            LoginController::class, "index"
+        ])->name("index");
+        // ログイン用コントローラー
+        Route::get("/login/{line_account_id}/{application_key}", [
+            LoginController::class, "detail"
+        ])->name("detail");
+    });
+
+
+    Route::group(["prefix" => "callback", "as" => "callback."], function () {
+        // ---------------------------------------------------------------
+        // LINEドメイン側で認証後に戻ってくるweb側のコールバックURL
+        // GETパラメータで line_account_idというキーでline_accounts.idを渡す
+        // ---------------------------------------------------------------
+        Route::get("/callback", [CallbackController::class, "index"])->name("index");
+        // ---------------------------------------------------------------
+        // LINE認証完了後,DBを更新後表示するLINEログイン完了ページ
+        // GETパラメータで line_account_idというキーでline_accounts.idを渡す
+        // ---------------------------------------------------------------
+        Route::get("/callback/completed", [CallbackController::class, "completed"])->name("completed");
+    });
+
     // LINEアカウントからログアウトするURL
     Route::get("/logout", [LogoutController::class, "index"])->name("logout.index");
 });
 
 
-// -------------------------------------------------------------------------
-// API用ルーティンググループ
-// あとでapi.phpに分ける
-// -------------------------------------------------------------------------
-Route::group(["prefix" => "api", "as" => "api."], function () {
-    Route::group(["prefix" => "line", "as" => "line."], function () {
-
-
-
-
-
-        // 修正後は以下
-        Route::group(["prefix" => "reserve", "as" => "reserve."], function () {
-            // 指定したLINEユーザーにメッセージをPushする
-            Route::post("/push/{line_reserve_id}", [ReserveController::class, "push"])->name("push");
-            // 任意のメッセージを作成および予約日時を指定する
-            Route::post("/reserve/{line_account_id}", [ReserveController::class, "reserve"])->name("reserve");
-            // リクエスト時点で未送信のメッセージ一覧を取得する
-            Route::get("unsent/{line_account_id}", [ReserveController::class, "unsentMessages"])->name("unsent");
-            // リクエスト時点で配信済みのメッセージ一覧を取得する
-            Route::get("sent/{line_account_id}", [ReserveController::class, "sentMessages"])->name("sent");
-        });
-
-
-
-    });
-});
+// // -------------------------------------------------------------------------
+// // API用ルーティンググループ
+// // あとでapi.phpに分ける
+// // -------------------------------------------------------------------------
+// Route::group(["prefix" => "api", "as" => "api."], function () {
+//     Route::group(["prefix" => "line", "as" => "line."], function () {
+//
+//
+//
+//
+//
+//         // 修正後は以下
+//         Route::group(["prefix" => "reserve", "as" => "reserve."], function () {
+//             // 指定したLINEユーザーにメッセージをPushする
+//             Route::post("/push/{line_reserve_id}", [ReserveController::class, "push"])->name("push");
+//             // 任意のメッセージを作成および予約日時を指定する
+//             Route::post("/reserve/{line_account_id}", [ReserveController::class, "reserve"])->name("reserve");
+//             // リクエスト時点で未送信のメッセージ一覧を取得する
+//             Route::get("unsent/{line_account_id}", [ReserveController::class, "unsentMessages"])->name("unsent");
+//             // リクエスト時点で配信済みのメッセージ一覧を取得する
+//             Route::get("sent/{line_account_id}", [ReserveController::class, "sentMessages"])->name("sent");
+//         });
+//
+//
+//
+//     });
+// });
